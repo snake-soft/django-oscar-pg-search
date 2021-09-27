7dc166620dd00730ee6ab1f2d977560577ee561e
Snake-Soft <info@snake-soft.com>
2021-09-27 16:07:06 +0200

    first test

--- tests/test_search_handler.py
+++ tests/test_search_handler.py
@@ -1,7 +1,16 @@
 from django.test.testcases import TestCase
 from oscar_pg_search.postgres_search_handler import PostgresSearchHandler
+from django.test.client import RequestFactory
+from oscar.apps.catalogue import views
+from oscar_pg_search.mixins import SearchViewMixin
+
+
+class CatalogueView(views.CatalogueView, SearchViewMixin):
+    pass
 
 
 class TestSearchHandler(TestCase):
+
     def test_instance(self):
-        self.assertEqual(True, True)
+        result = PostgresSearchHandler.normalize_query('query_string')
+        self.assertIsInstance(result, list)
